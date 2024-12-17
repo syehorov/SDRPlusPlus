97a4dc7affeb5243ffd635b2fd4b9f491bfc7bf8
Serhii Yehorov <syehorov@unixzone.org.ua>
2024-12-17 20:23:21 +0200

    update to build tetra plugin

--- sdrpp_module.cmake
+++ sdrpp_module.cmake
@@ -1,5 +1,32 @@
 # Get needed values depending on if this is in-tree or out-of-tree
 if (NOT SDRPP_CORE_ROOT)
+    set(SDRPP_CORE_ROOT "/usr/include/sdrpp_core")
+endif ()
+if (NOT SDRPP_MODULE_COMPILER_FLAGS)
+    # Compiler flags
+    if (${CMAKE_BUILD_TYPE} MATCHES "Debug")
+        # Debug Flags
+        if (MSVC)
+            set(SDRPP_MODULE_COMPILER_FLAGS /std:c++17 /EHsc)
+        elseif (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+            set(SDRPP_MODULE_COMPILER_FLAGS -g -Og -std=c++17 -Wno-unused-command-line-argument -undefined dynamic_lookup)
+        else ()
+            set(SDRPP_MODULE_COMPILER_FLAGS -g -Og -std=c++17)
+        endif ()
+    else()
+        # Normal Flags
+        if (MSVC)
+            set(SDRPP_MODULE_COMPILER_FLAGS /O2 /Ob2 /std:c++17 /EHsc)
+        elseif (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+            set(SDRPP_MODULE_COMPILER_FLAGS -O3 -std=c++17 -Wno-unused-command-line-argument -undefined dynamic_lookup)
+        else ()
+            set(SDRPP_MODULE_COMPILER_FLAGS -O3 -std=c++17)
+        endif ()
+    endif()
+endif ()
+
+# Get needed values depending on if this is in-tree or out-of-tree
+if (NOT SDRPP_CORE_ROOT)
     set(SDRPP_CORE_ROOT "@SDRPP_CORE_ROOT@")
 endif ()
 if (NOT SDRPP_MODULE_COMPILER_FLAGS)
