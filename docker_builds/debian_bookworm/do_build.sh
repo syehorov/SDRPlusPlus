9661b68469c87838918055f19696334ccdf6e1ad
Serhii Yehorov <syehorov@unixzone.org.ua>
2024-12-18 20:15:53 +0200

    Roll-back build method

--- docker_builds/debian_bookworm/do_build.sh
+++ docker_builds/debian_bookworm/do_build.sh
@@ -52,4 +52,28 @@ cmake .. -DOPT_BUILD_BLADERF_SOURCE=ON -DOPT_BUILD_LIMESDR_SOURCE=ON -DOPT_BUILD
 make VERBOSE=1 -j2
 
 cd ..
-sh make_debian_package.sh ./build 'libfftw3-dev, libglfw3-dev, libvolk2-dev, librtaudio-dev, libzstd-dev'
+mkdir sdrpp_debian_amd64
+mkdir sdrpp_debian_amd64/DEBIAN
+
+# Create package info
+echo Create package info
+echo Package: sdrpp >> sdrpp_debian_amd64/DEBIAN/control
+echo Version: 1.2.1$BUILD_NO >> sdrpp_debian_amd64/DEBIAN/control
+echo Maintainer: Serhii Yehorov >> sdrpp_debian_amd64/DEBIAN/control
+echo Architecture: all >> sdrpp_debian_amd64/DEBIAN/control
+echo Description: Bloat-free SDR receiver software >> sdrpp_debian_amd64/DEBIAN/control
+echo Depends: libiio0, libad9361-0, libhackrf0, libairspy0, libairspyhf1, librtaudio7, librtlsdr0, libbladerf2, liblimesuite23.11-1, libuhd4.7.0, libglfw3, sdrplay-api, libsoapysdr0.8 >> sdrpp_debian_amd64/DEBIAN/control
+
+# Copying files
+ORIG_DIR=$PWD
+cd $1
+make install DESTDIR=$ORIG_DIR/sdrpp_debian_amd64
+cd $ORIG_DIR
+
+# Create package
+echo Create package
+dpkg-deb --build sdrpp_debian_amd64
+
+# Cleanup
+echo Cleanup
+rm -rf sdrpp_debian_amd64
